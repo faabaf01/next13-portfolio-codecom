11b4d9ad0a06fc1d4e48a3e4f13b785aec6d115f
faabaf01 <farasya15@gmail.com>
2023-09-08 10:05:02 +0800

    edited Main margin top

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -7,9 +7,9 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+      <div className="max-w-fit w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-sm tracking-widest text-[#508991]">
+          <p className="uppercase text-sm tracking-widest mt-10 text-[#508991]">
             A LITTLE BIT ABOUT ME
           </p>
           <h1 className="py-4 text-gray-700">
